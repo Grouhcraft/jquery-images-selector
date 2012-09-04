cae0cb2660ee1f851ef96d3378d3e21dd798b613
Aristodemo Damien <knoodrake@gmail.com>
2012-09-04 18:35:06 +0200

    bugfixs (but new visual bugs for chrome..)

--- jquery-images-selector.js
+++ jquery-images-selector.js
@@ -2,7 +2,7 @@
     $.fn.ImagesSelector = function(options) {
         options = $.extend({}, $.fn.ImagesSelector.defaults, options);
         return this.each(function() {
-			$(this).addClass('j-im-sel');
+            $(this).addClass('j-im-sel');
             $('img', this).each(function() {
 
                 var imgBox = $('<div class="imgBox"></div>');
@@ -23,7 +23,9 @@
                 $(this).remove();
 
                 if (options.hideCheckbox) {
-                    cb_cont.css('visibility', 'hidden');
+                    //TODO: Check in IE
+                    //cb_cont.css('visibility', 'hidden');
+                    cb_cont.css('display', 'none');
                 }
 
                 overlay.css({
@@ -35,14 +37,13 @@
                 cb.attr('id', img.attr('id') + '_cb');
                 label.attr('for', cb.attr('id'));
 
-                overlay.css('top', '-' + img.height() + 'px');
-				cb_cont.css('bottom', img.outerHeight() + 'px');
-                cb_cont.css('right', (img.outerWidth() - cb_cont.outerWidth()) + 'px');
+                var top = Math.round(img.outerHeight() / 2) + overlay.outerHeight();
+                overlay.css('top', '-' + top + 'px');
 
-                if (options.selectedText) {
-                    overlay.html(options.selectedText);
-                } else {
+                if (options.useImagesTitles) {
                     overlay.html(img.attr('title'));
+                } else {
+                    overlay.html(options.selectedText);
                 }
 
                 if (options.bounce) {
@@ -60,7 +61,7 @@
                         }, options.bounceDuration, function() {});
                     });
                 }
-
+                    
                 cb.on('change', function() {
                     var subCont = $(this).parent().parent().parent();
                     var img = $('img', subCont);
@@ -75,6 +76,9 @@
                     }
                 });
 
+                overlay.on('click', function() {
+                    $(this).parent().find('label').click();
+                });
 
                 if ($.browser.msie && parseInt($.browser.version, 10) < 9) {
                     label.on('click', function() {
@@ -93,7 +97,8 @@
         });
     };
     $.fn.ImagesSelector.defaults = {
-        selectedText: false,
+        selectedText: 'selected',
+        useImagesTitles: false,
         hideCheckbox: true,
         bounceHeight: '10px',
         bounceDuration: 200,
